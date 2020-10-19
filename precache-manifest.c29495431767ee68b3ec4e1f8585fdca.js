5e58f04a61c6679a9d859cf11e30f17d81af009b
cleverCoader <adventure.developer45@gmail.com>
2020-10-19 20:34:28 +0530

    Updates

---
+++ precache-manifest.c29495431767ee68b3ec4e1f8585fdca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c8fceb040cdc9e8a61589558202826",
+    "url": "/alan-ai-news-app/index.html"
+  },
+  {
+    "revision": "1115543fbb9ba4f60209",
+    "url": "/alan-ai-news-app/static/css/main.a5d1c59f.chunk.css"
+  },
+  {
+    "revision": "622185653ea487704ca3",
+    "url": "/alan-ai-news-app/static/js/2.d60e7fe3.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/alan-ai-news-app/static/js/2.d60e7fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1115543fbb9ba4f60209",
+    "url": "/alan-ai-news-app/static/js/main.66ba8fae.chunk.js"
+  },
+  {
+    "revision": "be1e0895a58dfffc6e74",
+    "url": "/alan-ai-news-app/static/js/runtime-main.e90d1a4f.js"
+  }
+]);
