c759d7d04f109dea0a1ce083e989c89272ad2e8e
cleverCoader <adventure.developer45@gmail.com>
2020-10-20 00:50:11 +0530

    got it half done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React , {useState, useEffect}from 'react';
+import { Typography } from '@material-ui/core';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
 import NewsCards from './components/NewsCards/NewsCards';
 
+import useStyles from './style.js';
 
 const alanKey='b0e2a16edaa47db6fd4b71d5eee265232e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App=()=>{
 
   const [newsArticles, setNewsArticles]= useState([]);
+
+
+  const classes =useStyles();
 
   useEffect(()=>{
     alanBtn({
@@ -25,7 +30,15 @@ const App=()=>{
 
   return(
     <div>
-      <h1>Alan AI News</h1>
+        <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
       <NewsCards  articles={newsArticles} />
     </div>
   )
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+// import classes from '*.module.css';
+
+import useStyles from './style.js';
 
 const NewsCard = ({article :{description, publishedAt, source, title, url, urlToImage,} , i}) => {
 
-
+        const classes= useStyles();
+        const imageUrl ='https://wallpapercave.com/wp/wp1894272.jpg';
     return (
         <div>
             {/* <Card>
@@ -23,19 +27,19 @@ const NewsCard = ({article :{description, publishedAt, source, title, url, urlTo
                     <Typography variant="h5" color="textSecondary"> {i+1} </Typography>
                 </CardActions>
             </Card> */}
-            <Card>
-                <CardActionArea>
-                    <CardMedia image={urlToImage} />
-                    <div>
+            <Card className={classes.card}>
+                <CardActionArea href={url} target="_blank" >
+                    <CardMedia className={classes.media}   image={imageUrl} />
+                    <div className={classes.details}>
                         <Typography  variant="body2" color="textSecondary" component="h2"> {"19th oct 2020"} </Typography>
                         <Typography  variant="body2" color="textSecondary" component="h2"> BBC </Typography>
                     </div>
-                    <Typography gutterBottom variant="h5"> {"Ab Devilliars Hits High against RR in IPL 2020"} </Typography>
+                    <Typography className={classes.title} gutterBottom variant="h5"> {"Ab Devilliars Hits High against RR in IPL 2020"} </Typography>
                     <CardContent>
                         <Typography  variant="body2" color="textSecondary" component="p"> {"when RR played against RCB in ipl 2020 Thae amtch was so interesting that When AB came into crase The target was very far but he made It loos cool"} </Typography>
                     </CardContent>
                 </CardActionArea>
-                <CardActions>
+                <CardActions className={classes.cardActions}>
                     <Button size="small" color="primary"> Learn More </Button>
                     <Typography variant="h5" color="textSecondary"> {i+1} </Typography>
                 </CardActions>
