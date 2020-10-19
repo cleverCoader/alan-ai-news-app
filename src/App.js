f26b33ade8dd6449cf0d89e3979810ce190dd47e
cleverCoader <adventure.developer45@gmail.com>
2020-10-19 16:05:49 +0530

    simply superb This is amazing just with this much

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React , {useEffect}from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      ALAN AI WEB APPLICATION
-    </div>
-  );
-}
+// const alanKey='b0e2a16edaa47db6fd4b71d5eee265232e956eca572e1d8b807a3e2338fdd0dc/stage';
 
+const App=()=>{
+
+  useEffect(()=>{
+    alanBtn({
+      key: alanKey,
+      
+    })
+  },[])
+
+
+
+  return(
+    <div>
+      <h1>Alan AI News</h1>
+    </div>
+  )
+}
 export default App;
