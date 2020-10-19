0d33d69c51d0b54e95e5326d726e0a83b5cf0cfe
cleverCoader <adventure.developer45@gmail.com>
2020-10-19 17:04:41 +0530

    API call ? it worked

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React , {useEffect}from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
 
-// const alanKey='b0e2a16edaa47db6fd4b71d5eee265232e956eca572e1d8b807a3e2338fdd0dc/stage';
+const alanKey='b0e2a16edaa47db6fd4b71d5eee265232e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App=()=>{
 
   useEffect(()=>{
     alanBtn({
       key: alanKey,
-      
+      onCommand : ({command,  articles })=>{
+        if(command === 'newHeadlines'){
+          console.log(articles);
+        }
+      }
     })
   },[])
 
