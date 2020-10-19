3961ab8f54b1389d31b881f0a209561f9e05f493
green-code-developer <complexcodealgorithm@gmail.com>
2020-10-19 15:10:48 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      ALAN AI WEB APPLICATION
+    </div>
+  );
+}
+
+export default App;
